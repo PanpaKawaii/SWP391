f43d97710d4f0577959acb2824a42f56279b98bb
PanpaKawaii <dngngchitriu2004@gmail.com>
2024-10-22 00:08:42 +0700

    Fix Spelling 3

--- src/assets/listUTILITIes.js
+++ src/assets/listUTILITIes.js
@@ -1,10 +1,10 @@
-import id1 from './imageUTILITIs/1.jpg';
-import id2 from './imageUTILITIs/2.jpg';
-import id3 from './imageUTILITIs/3.jpg';
-import id4 from './imageUTILITIs/4.jpg';
-import id5 from './imageUTILITIs/5.jpg';
+import id1 from './imageUTILITIes/1.jpg';
+import id2 from './imageUTILITIes/2.jpg';
+import id3 from './imageUTILITIes/3.jpg';
+import id4 from './imageUTILITIes/4.jpg';
+import id5 from './imageUTILITIes/5.jpg';
 
-export const imageUTILITIs = [
+export const imageUTILITIes = [
     { id: 1, image: id1 },
     { id: 2, image: id2 },
     { id: 3, image: id3 },
