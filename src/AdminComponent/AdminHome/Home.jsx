cf1b898cc0d5052969cc3a220f54389d59e3d396
KimHang1504 <kimhangbt2017@gmail.com>
2024-11-02 14:09:29 +0700

    commit home

--- src/AdminComponent/AdminHome/Home.jsx
+++ src/AdminComponent/AdminHome/Home.jsx
@@ -3,15 +3,22 @@ import './HomeStyle.css';
 import api from '../api/axios';
 
 export default function Home() {
-  const [adminName, setAdminName] = useState('');
+  const [name, setName] = useState('');
   const [image, setImage] = useState('');
   const [error, setError] = useState(null);
+  const [id, setId] = useState(null);
+  
+  const UserId = localStorage.getItem('UserId');
+
+  useEffect(() => {
+    const UserIdInt = parseInt(UserId, 10);
+    setId(UserIdInt);
+  }, [UserId]);
 
   const fetchUser = async () => {
     try {
-      const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkYW5nbmdvY2hhaXRyaWV1QGdtYWlsLmNvbSIsImp0aSI6ImE5MmUwOTBkLTQ2NmEtNDE2My1hMDQ3LWUyOWNjYjExOGE2OCIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWVpZGVudGlmaWVyIjoiOSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFkbWluIiwiZXhwIjoxNzMzMDc1ODUxLCJpc3MiOiJQb2RCb29raW5nIiwiYXVkIjoiUG9kV2ViIn0.SljDy518ZlaoY5hp6kKZvBp3-j5vXItyHQ0H7Y0ik3o"; // Thay thế bằng token thực tế của bạn
-
-      const response = await api.get("User", {
+      const token = localStorage.getItem('token');
+      const response = await api.get(`User/${id}`, {
         headers: {
           Authorization: `Bearer ${token}`, // Thêm token vào header
         },
@@ -25,30 +32,30 @@ export default function Home() {
 
   useEffect(() => {
     const getUserData = async () => {
-      try {
-        const userData = await fetchUser(); // Gọi hàm để lấy thông tin người dùng
-        console.log("Dữ liệu người dùng:", userData); // Kiểm tra dữ liệu trả về
-        const adminUser = userData.find(user => user.role === 'Admin');
-        if (adminUser) {
-          setAdminName(adminUser.name);
-          setImage(adminUser.image);
-        } else {
-          console.error('No admin user found');
+      if (id) {
+        try {
+          const userData = await fetchUser(); // Gọi hàm để lấy thông tin người dùng
+          if (userData) {
+            setName(userData.name);
+            setImage(userData.image);
+          } else {
+            console.error('No admin user found');
+          }
+        } catch (error) {
+          setError('Error fetching users: ' + error.message);
+          console.error('Error fetching users:', error);
         }
-      } catch (error) {
-        setError('Error fetching users: ' + error.message);
-        console.error('Error fetching users:', error);
       }
     };
 
     getUserData();
-  }, []);
+  }, [id]);
 
   return (
     <div className="welcome-container">
       {error && <p className="error-message">{error}</p>}
       <img src={image} alt="Welcome" className="welcome-image" />
-      <h1>Chào mừng, {adminName}!</h1>
+      <h1>Chào mừng, {name}!</h1>
       <p>Chúng tôi rất vui được có bạn ở đây. Hãy khám phá các chức năng bên cạnh!</p>
     </div>
   );
