1246e86d01a064572f3985b89eba108c4a9d9a52
PanpaKawaii <dngngchitriu2004@gmail.com>
2024-11-02 14:53:42 +0700

    Fix My Booking Button

--- src/UserComponent/Booking/BookingPodContent.jsx
+++ src/UserComponent/Booking/BookingPodContent.jsx
@@ -8,6 +8,13 @@ import { imagePODs } from '../../assets/listPODs';
 
 export default function BookingPodContent() {
 
+    const [id, setId] = useState(null);
+    const UserId = localStorage.getItem('UserId');
+    useEffect(() => {
+        const UserIdInt = parseInt(UserId, 10);
+        setId(UserIdInt);
+    }, [UserId]);
+
     const [StoreId, setStoreId] = useState(null);
     const { pathname } = useLocation();
     console.log(pathname);
@@ -218,7 +225,7 @@ export default function BookingPodContent() {
 
                 </Form>
 
-                <Link to={`../user/booking`}><Button className='btn'>POD ĐÃ ĐẶT</Button></Link>
+                {id ? <Link to={`../user/booking`}><Button className='btn'>POD ĐÃ ĐẶT</Button></Link> : <></>}
 
             </div>
 
