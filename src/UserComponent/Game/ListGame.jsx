babed65101c51d89bc415ce0052c76019046c5e8
PanpaKawaii <dngngchitriu2004@gmail.com>
2024-12-01 18:05:35 +0700

    Style List Game

--- src/UserComponent/Game/ListGame.jsx
+++ src/UserComponent/Game/ListGame.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import './ListGame.css';
 
+import MinesweeperSRC from '../../assets/imageGAMEs/Minesweeper.png';
+import WhatIsThePasswordSRC from '../../assets/imageGAMEs/Minesweeper.png';
+
 export default function ListGame() {
     return (
         <div className='listgame-container'>
-            <h1>List Game</h1>
+            <h1><b>List Game</b></h1>
 
-            <Link to='/game/minesweeper'><i className='fa-solid fa-bomb'></i>Minesweeper</Link>
-            <Link to='/game/whatisthepassword'><i className='fa-solid fa-code'></i>WhatIsThePassword</Link>
+            <div className='listgame'>
+                <Link to='/game/minesweeper'>
+                    <img src={MinesweeperSRC} alt='Minesweeper'></img>
+                    <h3><b>Minesweeper</b></h3>
+                </Link>
+                <Link to='/game/whatisthepassword'>
+                    <img src='' alt='Minesweeper'></img>
+                    <h3><b>What Is The Password?</b></h3>
+                </Link>
+            </div>
         </div>
     )
 }
