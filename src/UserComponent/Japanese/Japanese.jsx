eec97b7315027a11bcd262efcdaba8f771d24d3c
PanpaKawaii <dngngchitriu2004@gmail.com>
2025-01-05 10:29:22 +0700

    Add Kanji Words

--- src/UserComponent/Japanese/Japanese.jsx
+++ src/UserComponent/Japanese/Japanese.jsx
@@ -3,6 +3,9 @@ import { useState, useEffect } from 'react';
 import { Table, Button, Form } from 'react-bootstrap';
 import './Japanese.css';
 
+import { Kanji } from '../../assets/listJapanese';
+import { KanjiExample } from '../../assets/listJapanese';
+
 export default function Japanese() {
 
 
@@ -14,6 +17,49 @@ export default function Japanese() {
 
             <div className='game-content'>
                 <h2>Japanese Content</h2>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Character</th>
+                            <th>Sino-Vietnamese</th>
+                            <th>On</th>
+                            <th>Kun</th>
+                            <th>Meaning</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {Kanji.map((kanji_array, k) => (
+                            <tr key={k}>
+                                <td style={{ color: kanji_array.Id == 'Unknown' && 'red' }}>{kanji_array.Id}</td>
+                                <td style={{ color: kanji_array.SinoVietnamese == 'Unknown' && 'red' }}>{kanji_array.SinoVietnamese}</td>
+                                <td style={{ color: kanji_array.On == 'Unknown' && 'red' }}>{kanji_array.On}</td>
+                                <td style={{ color: kanji_array.Kun == 'Unknown' && 'red' }}>{kanji_array.Kun}</td>
+                                <td style={{ color: kanji_array.Meaning == 'Unknown' && 'red' }}>{kanji_array.Meaning}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Character</th>
+                            <th>Word</th>
+                            <th>Hiragana</th>
+                            <th>Meaning</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {KanjiExample.map((example, e) => (
+                            <tr key={e}>
+                                <td>{example.Character}</td>
+                                <td>{example.Word}</td>
+                                <td>{example.Hiragana}</td>
+                                <td>{example.Meaning}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
             </div>
         </div>
     )
