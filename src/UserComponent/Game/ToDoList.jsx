e21d466700d271681c063cd307546f036f0f64d0
PanpaKawaii <dngngchitriu2004@gmail.com>
2025-01-12 22:11:17 +0700

    Delete and Change note

--- src/UserComponent/Game/ToDoList.jsx
+++ src/UserComponent/Game/ToDoList.jsx
@@ -4,187 +4,24 @@ import { Table, Button, Form } from 'react-bootstrap';
 import './ToDoList.css';
 
 export default function ToDoList() {
-
-    const [Maze, setMaze] = useState([
-        [2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-        [0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0],
-        [0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0],
-        [0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0],
-        [0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1],
-        [0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0],
-        [1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0],
-        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
-        [0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0],
-        [0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 3, 0, 1, 0, 0],
-        [1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
-        [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0],
-        [1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0],
-        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
-    ]);
-
-    const [Maze1, setMaze1] = useState([
-        [2, 1, 1],
-        [0, 1, 3],
-        [0, 0, 0]
-    ]);
-
-    const [Maze2, setMaze2] = useState([
-        [2, 0, 0, 0, 1, 0, 0, 1, 0, 0],
-        [1, 1, 1, 0, 1, 0, 1, 1, 0, 1],
-        [0, 1, 0, 0, 0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
-        [1, 1, 0, 1, 0, 1, 0, 0, 1, 0],
-        [0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
-        [0, 1, 1, 1, 0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-        [1, 0, 1, 1, 0, 1, 0, 1, 1, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 3]
-    ]);
-
-    const [Maze3, setMaze3] = useState([
-        [0, 2, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 1, 0, 1, 1, 1, 1, 0],
-        [0, 1, 0, 1, 0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 1, 0, 1, 0, 1, 1, 1],
-        [0, 1, 0, 1, 0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 1, 0, 1, 1, 1, 1, 0],
-        [0, 1, 0, 1, 0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 1, 0, 1, 1, 1],
-        [0, 1, 1, 1, 1, 1, 0, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 3]
-    ]);
-
-    const [Maze4, setMaze4] = useState([
-        [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3]
-    ]);
-
-    const [Maze5, setMaze5] = useState([
-        [2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-        [0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0],
-        [0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0],
-        [0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0],
-        [0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1],
-        [0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0],
-        [1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0],
-        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
-        [0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0],
-        [0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 3, 0, 1, 0, 0],
-        [1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
-        [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0],
-        [1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0],
-        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
-    ]);
-
-    const [VisitedCells, setVisitedCells] = useState(Array(Maze.length).fill(0).map(() => Array(Maze[0].length).fill(false)));
-    const [Found, setFound] = useState(false);
-    const [Path, setPath] = useState([]);
     const [Refresh, setRefresh] = useState(0);
-
-    const setCurrentMaze = (MazeNumber) => {
-        let newMaze;
-        switch (MazeNumber) {
-            case 1:
-                newMaze = [...Maze1];
-                break;
-            case 2:
-                newMaze = [...Maze2];
-                break;
-            case 3:
-                newMaze = [...Maze3];
-                break;
-            case 4:
-                newMaze = [...Maze4];
-                break;
-            case 5:
-                newMaze = [...Maze5];
-                break;
-            default:
-                newMaze = [...Maze];
-                break;
-        }
-        setMaze(newMaze);
-        const newVisitedCells = Array(newMaze.length).fill(0).map(() => Array(newMaze[0].length).fill(false));
-        setVisitedCells(newVisitedCells);
-        setFound(false);
-        setPath([]);
-    }
-
-    useEffect(() => {
-        const newVisitedCells = Array(Maze.length).fill(0).map(() => Array(Maze[0].length).fill(false));
-        setVisitedCells(newVisitedCells);
-        setFound(false);
-        setPath([]);
-    }, [Refresh]);
-
-    const visitCell = async (row, col, Path) => {
-        const newVisitedCells = [...VisitedCells];
-        const newMaze = [...Maze];
-        if (newMaze[row][col] === 1 || newVisitedCells[row][col] === true) {
-            console.log('Refuse');
-            return;
-        }
-        console.log('Activate');
-
-        const newPath = [...Path, [row, col]];
-        console.log('Path: ', newPath);
-
-        newVisitedCells[row][col] = true;
-        setVisitedCells(newVisitedCells);
-
-        if (newMaze[row][col] === 3) {
-            console.log('Found');
-            console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-            setPath(newPath);
-            setFound(true);
-            return;
-        }
-
-        const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-        await sleep(100);
-
-        if (newMaze[row][col] !== 1) {
-            const directions = [[1, 0], [0, 1], [-1, 0], [0, -1]];
-            for (let direction of directions) {
-                const newRow = row + direction[0];
-                const newCol = col + direction[1];
-                if (newRow >= 0 && newRow < Maze.length && newCol >= 0 && newCol < Maze[0].length) {
-                    visitCell(newRow, newCol, newPath);
-                    // await visitCell(newRow, newCol, newPath);
-                }
-            }
-        }
-    }
-
-
-
+    const [newNoteId, setNewNoteId] = useState(null);
     const [List, setList] = useState([{
         id: null,
-        isDone: '',
-        content: '',
-        dateCreate: '',
-        lastChange: ''
+        isDone: null,
+        content: null,
+        dateCreate: null,
+        lastChange: null
     }])
 
     useEffect(() => {
         const ListFromStorage = [];
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 100; i++) {
             const isDone = localStorage.getItem(`isDone${i}`);
             const content = localStorage.getItem(`content${i}`);
             const dateCreate = localStorage.getItem(`dateCreate${i}`);
             const lastChange = localStorage.getItem(`lastChange${i}`);
-            if (content) {
+            if (content !== null) {
                 ListFromStorage.push({
                     id: i,
                     isDone: isDone,
@@ -194,10 +31,10 @@ export default function ToDoList() {
                 });
             }
         }
-        setList(ListFromStorage);
-    }, []);
+        setList(ListFromStorage.sort((b, a) => a.id - b.id));
+    }, [Refresh]);
 
-    const AddingNote = (content) => {
+    const AddNote = (content) => {
         let maxId = 0;
         if (List.length > 0) {
             maxId = Math.max(...List.map(item => item.id));
@@ -209,16 +46,50 @@ export default function ToDoList() {
             dateCreate: new Date().toISOString(),
             lastChange: new Date().toISOString()
         };
-        setList(prevList => [...prevList, newNote]);
         localStorage.setItem(`isDone${maxId + 1}`, newNote.isDone);
         localStorage.setItem(`content${maxId + 1}`, newNote.content);
         localStorage.setItem(`dateCreate${maxId + 1}`, newNote.dateCreate);
         localStorage.setItem(`lastChange${maxId + 1}`, newNote.lastChange);
+        setNewNoteId(newNote.id);
+        setList(prevList => [...prevList, newNote]);
+        setRefresh(Refresh + 1);
+    }
+
+    const DeleteNote = (id) => {
+        localStorage.removeItem(`isDone${id}`);
+        localStorage.removeItem(`content${id}`);
+        localStorage.removeItem(`dateCreate${id}`);
+        localStorage.removeItem(`lastChange${id}`);
+        setRefresh(Refresh + 1);
+    }
+
+    const EditCheckBox = (id, checked) => {
+        const newNote = {
+            isDone: checked === true ? 'Done' : 'NotDone',
+            lastChange: new Date().toISOString()
+        };
+        localStorage.setItem(`isDone${id}`, newNote.isDone);
+        localStorage.setItem(`lastChange${id}`, newNote.lastChange);
+        setRefresh(Refresh + 1);
+    }
+
+    const EditNote = (id, content) => {
+        const newNote = {
+            content: content,
+            lastChange: new Date().toISOString()
+        };
+        localStorage.setItem(`content${id}`, newNote.content);
+        localStorage.setItem(`lastChange${id}`, newNote.lastChange);
+        setRefresh(Refresh + 1);
+    }
+
+    const handleEdittingNote = (e) => {
+        e.preventDefault();
     }
 
     const handleAddingNote = (e) => {
         e.preventDefault();
-        AddingNote(e.target.addingnote.value);
+        AddNote(e.target.addingnote.value);
         e.target.addingnote.value = '';
     }
 
@@ -228,123 +99,60 @@ export default function ToDoList() {
                 <h1><b>To Do List</b></h1>
             </div>
 
-            <div>
-
-                <Form onSubmit={handleAddingNote}>
+            <div className='todolist-content'>
+                <Form className='add-form' onSubmit={handleAddingNote}>
                     <Form.Group controlId='addingnote' className='form-group'>
                         <Form.Control type='text' placeholder='Add note' />
                     </Form.Group>
                     <Button type='submit' className='btn'>ADD</Button>
-                    <Button type='reset' className='btn btn-reset'>CLEAR TEXT</Button>
-                    <Button className='btn btn-reset' onClick={() => setRefresh(Refresh + 1)}>RESET</Button>
                 </Form>
 
                 <div>
-                    <Table striped bordered hover>
+                    <Table striped bordered hover className='no-wrap align-middle table'>
                         <thead>
                             <tr>
-                                <td>ID</td>
-                                <th>Done?</th>
-                                <th>Content</th>
+                                <th>Index</th>
+                                {/* <td>ID</td> */}
+                                <th className='content-cell'>Content</th>
                                 <th>Date Created</th>
-                                <th>Date Changed</th>
+                                <th>Latest Change</th>
                                 <th>Actions</th>
                             </tr>
                         </thead>
                         <tbody>
                             {List.map((item, index) => (
-                                <tr key={index}>
-                                    <td>{item.id}</td>
-                                    <td>{item.isDone === 'Done' ? 'Done' : 'Not Done'}</td>
-                                    <td>{item.content}</td>
+                                <tr key={index} className={item.id === newNoteId ? 'fade-in' : ''}>
+                                    <td>{index + 1}</td>
+                                    {/* <td>{item.id}</td> */}
+                                    <td>
+                                        <Form className='edit-form' onSubmit={handleEdittingNote}>
+                                            <Form.Group controlId='edittingcheckbox' className='form-group formcheckbox'>
+                                                <Form.Check type='checkbox' checked={item.isDone === 'Done'} onChange={(e) => EditCheckBox(item.id, e.target.checked)} />
+                                            </Form.Group>
+                                            <Form.Group controlId='edittingnote' className='form-group forminput'>
+                                                <Form.Control type='text' placeholder='...' value={item.content} onChange={(e) => EditNote(item.id, e.target.value)}
+                                                    style={{ color: item.isDone === 'Done' && '#cccccc' }} />
+                                            </Form.Group>
+                                        </Form>
+                                    </td>
                                     <td>{item.dateCreate}</td>
                                     <td>{item.lastChange}</td>
                                     <td>
-                                        <Button onClick={() => changeNote(index, item.id)}>Change</Button>
-                                        <Button onClick={() => deleteNote(index, item.id)}>Delete</Button>
+                                        <Button className='btn' onClick={() => DeleteNote(item.id)}>DELETE</Button>
                                     </td>
                                 </tr>
                             ))}
+                            {List.length === 0 &&
+                                <tr>
+                                    <td>NONE</td>
+                                    <td>NONE</td>
+                                    <td>NONE</td>
+                                    <td>NONE</td>
+                                    <td>NONE</td>
+                                </tr>}
                         </tbody>
                     </Table>
                 </div>
-            </div>
-
-            <div className='todolist-content'>
-
-                <h2><b>Animation Calculation</b></h2>
-                <Table
-                    className='no-wrap align-middle table'
-                // style={{ '--table-width': 10, }}
-                >
-                    <tbody style={{ cursor: 'pointer', userSelect: 'none' }}>
-                        {Maze.map((row, index_row) => (
-                            <tr key={index_row}>
-                                {row.map((cell, index_col) => (
-                                    <td key={index_col}
-                                        style={{
-                                            backgroundColor: cell === 2 ?
-                                                '#dc3545'
-                                                :
-                                                (cell === 3 ?
-                                                    '#ffc107'
-                                                    :
-                                                    (cell === 0 ?
-                                                        '#28a745'
-                                                        :
-                                                        '#fb8b24'
-                                                    )
-                                                ),
-                                            color: VisitedCells[index_row][index_col] === true && '#ffffff',
-                                        }}
-                                        onClick={() => { visitCell(index_row, index_col, Path) }}
-                                    >
-                                        <p>{index_row}-{index_col}</p>
-                                    </td>
-                                ))}
-                            </tr>
-                        ))}
-                    </tbody>
-                </Table>
-
-                {Found &&
-                    <>
-                        <h2><b>Final Path</b></h2>
-                        <Table
-                            className='no-wrap align-middle table'
-                        // style={{ '--table-width': 10, }}
-                        >
-                            <tbody className='tablebody'>
-                                {Maze.map((row, index_row) => (
-                                    <tr key={index_row}>
-                                        {row.map((cell, index_col) => (
-                                            <td key={index_col}
-                                                style={{
-                                                    backgroundColor: cell === 2 ?
-                                                        '#dc3545'
-                                                        :
-                                                        (cell === 3 ?
-                                                            '#ffc107'
-                                                            :
-                                                            (cell === 0 ?
-                                                                '#28a745'
-                                                                :
-                                                                '#fb8b24'
-                                                            )
-                                                        ),
-                                                    color: Path.some(path => path[0] === index_row && path[1] === index_col) ? '#ffffff' : '',
-                                                }}
-                                                onClick={() => { visitCell(index_row, index_col, Path) }}
-                                            >
-                                                <p>{index_row}-{index_col}</p>
-                                            </td>
-                                        ))}
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </Table>
-                    </>
-                }
             </div>
         </div>
     )
