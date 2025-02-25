d470e8f8b8eeb13d169a3ba080a77e04fd3d4db9
PanpaKawaii <dngngchitriu2004@gmail.com>
2025-02-25 12:27:58 +0700

    Add Dodecahedron

--- src/UserComponent/Game/TestObject.jsx
+++ src/UserComponent/Game/TestObject.jsx
@@ -11,7 +11,7 @@ export default function TestObject() {
                 </div>
             </div>
 
-            <div class="scene-cube">
+            <div class='scene-cube'>
                 <div class='cube'>
                     <div class='face front'>Front</div>
                     <div class='face back'>Back</div>
@@ -22,12 +22,33 @@ export default function TestObject() {
                 </div>
             </div>
 
-            <div class="scene-pyramid">
-                <div class="pyramid">
-                    <div class="face front">Red</div>
-                    <div class="face left">Green</div>
-                    <div class="face right">Blue</div>
-                    <div class="face bottom">Yellow</div>
+            <div class='scene-pyramid'>
+                <div class='pyramid'>
+                    <div class='face front'>Red</div>
+                    <div class='face left'>Green</div>
+                    <div class='face right'>Blue</div>
+                    <div class='face bottom'>Yellow</div>
+                </div>
+            </div>
+
+            <div class='scene-dodecahedron'>
+                <div class='dodecahedron'>
+                    <div class='face f1'>
+                        {/* <button style={{ backgroundColor: 'red', width: '120px' }}> */}
+                            Face1
+                        {/* </button> */}
+                    </div>
+                    <div class='face f2'>Face2</div>
+                    <div class='face f3'>Face3</div>
+                    <div class='face f4'>Face4</div>
+                    <div class='face f5'>Face5</div>
+                    <div class='face f6'>Face6</div>
+                    <div class='face f7'>Face7</div>
+                    <div class='face f8'>Face8</div>
+                    <div class='face f9'>Face9</div>
+                    <div class='face f10'>Face10</div>
+                    <div class='face f11'>Face11</div>
+                    <div class='face f12'>Face12</div>
                 </div>
             </div>
         </div>
